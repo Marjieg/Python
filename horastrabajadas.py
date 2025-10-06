d5ed42f3155b8d4b0e53923619c3e9db5ff69aa0
Marina-J.Egea <marinajegea@gmail.com>
2025-10-06 12:44:04 +0200

    sumaenteros

--- horastrabajadas.py
+++ horastrabajadas.py
@@ -1,4 +1,4 @@
 horas = float(input('¿Cuántas horas has trabajado?'))
 preciohora = 9
-paga = horas*preciohora
-print ('te corresponden',(paga),'euros')
+paga = (horas*preciohora)
+print ('te corresponde',(paga),'euros')
---
+++ sumaenteros.py
@@ -0,0 +1,3 @@
+n = int(input('introduce un número entero positivo'))
+suma = (n*(n+1))
+print (suma/2)
