7c1096722860b97118cdf0fb942407ab875f482f
Marina-J.Egea <marinajegea@gmail.com>
2025-10-09 10:56:46 +0200

    multiplicacadena

---
+++ ejerciciosdecadenas/cadenalineas.py
@@ -0,0 +1,8 @@
+#Escribir un programa que pregunte el nombre del usuario en la consola y un número
+#entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces
+#como el número introducido
+
+nombre = (input('¿cuál es tu nombre?'))
+entero = int(input('introduce un número entero'))
+
+print ((nombre + "\n") * int(entero))
