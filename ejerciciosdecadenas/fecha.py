def0227ca119a3bc8cbb60952f52b597704f9a3b
Marina-J.Egea <marinajegea@gmail.com>
2025-10-16 10:38:09 +0200

    fechanacimiento

--- ejerciciosdecadenas/fecha.py
+++ ejerciciosdecadenas/fecha.py
@@ -1,11 +1,24 @@
 #Ejercicio 9
 #Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato
-#dd/mm/aaaa y muestra por pantalla, el día, el mes y el año. Adaptar el programa
-#anterior para que también funcione cuando el día o el mes se introduzcan con un
-#solo carácter.
+#dd/mm/aaaa y muestra por pantalla, el día, el mes y el año. 
 
-fecha = input('Indtroduzca su fecha de nacimiento dd/mm/aaaa')
+fecha = input('Introduzca su fecha de nacimiento dd/mm/aaaa')
 
 print('Día', fecha[:2])
 print('Mes', fecha[3:5])
 print('Año', fecha[6:])
+
+#Adaptar el programa anterior para que también funcione cuando el día o el mes se introduzcan con un
+#solo carácter.
+
+fecha = input('Introduzca su fecha de nacimiento')
+
+
+fecha = input('Introduzca su fecha de nacimiento dd/mm/aaaa')
+dia = fecha[:fecha.find('/')]
+mesaño = fecha[fecha.find('/')+1:]
+mes = mesaño[:mesaño.find('/')]
+año = mesaño[mesaño.find('/')+1:]
+print('Día', dia)
+print('Mes', mes)
+print('Año', año)
