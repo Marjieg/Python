c6f56640f5b1387791734138ca203af5fc6b8c2a
Marina-J.Egea <marinajegea@gmail.com>
2025-10-16 10:24:44 +0200

    fecha

--- ejerciciosdecadenas/cadenanumeros.py
+++ ejerciciosdecadenas/cadenanumeros.py
@@ -1,4 +1,4 @@
-#Escribir un programa que pregunte por consola el precio de un producto en euros
+#ejercicio 8.Escribir un programa que pregunte por consola el precio de un producto en euros
 #con dos decimales y muestre por pantalla el número de euros y el número de
 #céntimos del precio introducido.
 
