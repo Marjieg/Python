de2341c70aa9af012489682cab95b381a1941477
Marina-J.Egea <marinajegea@gmail.com>
2025-10-23 10:40:03 +0200

    ejercico fecha actualizado con split

--- ejerciciosdecadenas/fecha.py
+++ ejerciciosdecadenas/fecha.py
@@ -2,20 +2,20 @@
 #Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato
 #dd/mm/aaaa y muestra por pantalla, el día, el mes y el año. 
 
-fecha = input('Introduzca su fecha de nacimiento dd/mm/aaaa')
+#fecha = input('Introduzca su fecha de nacimiento dd/mm/aaaa')--> otra forma de hacerlo
 
-print('Día', fecha[:2])
-print('Mes', fecha[3:5])
-print('Año', fecha[6:])
+#print('Día', fecha[:2])
+#print('Mes', fecha[3:5])
+#print('Año', fecha[6:])
+
+#con split sería --> (mejor esta forma)
+
+fecha = input('Introduzca su fecha de nacimiento dd/mm/aaaa')
+x = fecha.split("/")
+print(f"el dia es {x[0]}, el mes es {x[1]} y el año {x[2]}")
+
+
 
 #Adaptar el programa anterior para que también funcione cuando el día o el mes se introduzcan con un
 #solo carácter.
-
-fecha = input('Introduzca su fecha de nacimiento dd/mm/aaaa')
-dia = fecha[:fecha.find('/')]
-mesaño = fecha[fecha.find('/')+1:]
-mes = mesaño[:mesaño.find('/')]
-año = mesaño[mesaño.find('/')+1:]
-print('Día', dia)
-print('Mes', mes)
-print('Año', año)
+# funciona igual con split
