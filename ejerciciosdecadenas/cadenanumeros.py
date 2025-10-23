7ea05be6a2614c26dd8cad2bedf7107887b7da69
Marina-J.Egea <marinajegea@gmail.com>
2025-10-23 10:50:11 +0200

    actualización cadenas

--- ejerciciosdecadenas/cadenanumeros.py
+++ ejerciciosdecadenas/cadenanumeros.py
@@ -3,4 +3,6 @@
 #céntimos del precio introducido.
 
 precio = input("Introduce el precio del producto con dos decimales:  ")
-print(precio[:precio.find(',')], 'euros y', precio[precio.find(',')+1:], 'céntimos.')
+x = precio.split(",")
+
+print (f"{x[0]} euros y {x[1]} centimos")
