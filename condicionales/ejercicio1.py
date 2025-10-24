a5e843a09ce4c8ca5b667a41a84115063614956b
Marina-J.Egea <marinajegea@gmail.com>
2025-10-24 14:06:52 +0200

    mayordeedad

---
+++ condicionales/ejercicio1.py
@@ -0,0 +1,10 @@
+#Ejercicio 1
+#Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es
+#mayor de edad o no.
+
+edad = float(input("dime tu edad"))
+
+if (edad >= 18) :
+    print ("eres mayor de edad")
+else:
+    print ("no eres mayor de edad")
