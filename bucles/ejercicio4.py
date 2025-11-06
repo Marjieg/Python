09612ef40db4ba566a7d731d3fd246c2cd5073eb
Marjieg <marinajegea@gmail.com>
2025-11-06 11:13:56 +0100

    ejercicio4 bucles

--- bucles/ejercicio4.py
+++ bucles/ejercicio4.py
@@ -2,3 +2,7 @@
 Escribir un programa que pida al usuario un número entero positivo y muestre por
 pantalla la cuenta atrás desde ese número hasta cero separados por comas"""
 
+num = int(input("introduce un número entero positivo"))
+
+for i in range(num,0,-1): # inicializo con el número que introduzco hasta que llegue a 0 y voy contando en -1
+    print(i)
