803bf83546f6fed4e2d0a75e66ddc745b30ffed5
Marjieg <marinajegea@gmail.com>
2025-11-11 21:09:42 +0100

    ejercicio13

---
+++ bucles/ejercicio13.py
@@ -0,0 +1,10 @@
+"""Ejercicio 13 Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta que el usuario escriba “salir” que terminará."""
+
+while True:
+    frase = input("introduce algo: ")
+    if frase == "salir":
+        break
+    print(frase)
+    
+
+    
