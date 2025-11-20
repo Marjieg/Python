fbbfe60f7c12b966525585372a214c7b43c8b4da
Marjieg <marinajegea@gmail.com>
2025-11-20 09:39:34 +0100

    ejer clase

--- ejerclase/ejercicio4.py
+++ ejerclase/ejercicio4.py
@@ -4,3 +4,8 @@ for i in range(0,a,1):
     for j in range(1,a,1):
         print('*',end=' ')
     print('*')
+
+#otra forma
+alt=int(input('introduce la altura'))
+for i in range(0,alt):
+    print('* '*alt)
---
+++ ejerclase/ejercicio5.py
@@ -0,0 +1,4 @@
+num=int(input('dime un número'))
+for i in range(1,11):
+    multi= i * num
+    print(f'{i} x {num} = {multi}')
