6f4bee5bf6c35ed5878533cc0df6bd838aa6f22e
Marina-J.Egea <marinajegea@gmail.com>
2026-01-08 12:33:18 +0100

    Ejer 5

---
+++ listasytuplas/Ejercicio5.py
@@ -0,0 +1,8 @@
+"""Ejercicio 5
+Escribir un programa que almacene en una lista los números del 1 al 10 y los
+muestre por pantalla en orden inverso separados por comas"""
+
+lista= [1,2,3,4,5,6,7,8,9,10]
+print (lista)
+lista.reverse() #invierto la lista con la función reverse
+print (f'ahora imprimo la lista al revés {lista}')
