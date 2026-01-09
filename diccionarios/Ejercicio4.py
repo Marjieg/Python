13010d02434cece08b3644b4df9cb7b3f465eed9
Marina-J.Egea <marinajegea@gmail.com>
2026-01-09 12:38:55 +0100

    ejer 4

---
+++ diccionarios/Ejercicio4.py
@@ -0,0 +1,9 @@
+"""Ejercicio 4
+Escribir un programa que pregunte una fecha en formato dd/mm/aaaa y muestre
+por pantalla la misma fecha en formato dd de <mes> de aaaa donde <mes> es
+el nombre del mes."""
+
+meses = {1:'enero',2:'febrero',3:'marzo',4:'abril',5:'mayo',6:'junio',7:'julio',8:'agosto',9:'septiembre',10:'octubre',11:'noviembre',12:'diciembre'}
+fecha = input('Dime una fecha en formato dd/mm/aaaa')
+fecha = fecha.split('/') #importante hacer esto para separar por /
+print(f'el dia es {fecha[0]} el mes es {meses[int(fecha[1])]} y el año es {fecha[2]}')
