5cd21956b99fd7614ccad6c984deac6013793941
Marjieg <marinajegea@gmail.com>
2026-01-22 09:55:15 +0100

    ejercicio5

--- funciones/ejercicio5.py
+++ funciones/ejercicio5.py
@@ -1,15 +1,22 @@
 """Escribir una función que calcule el área de un círculo y otra que calcule el volumen
 de un cilindro usando la primera función."""
 
-radio = int(input('introduce el radio del círculo: '))
-def area():
+#las funciones las ponemos al principio del código
+def area(radio):
     a = (3.141515) * (radio ** 2)
     return a
-print (area())
 
-h = int(input('introduce la altura del cilintro: '))
-def volumen():
-    v = (4*(area()) * h)/3
+
+def volumen(h):
+    v = (4*(area(radio)) * h)/3
     return v
-print (f'el volumen de un cilintro con altura {h} y radio {radio} es {volumen()}')
+
+
+# la declaración de variables globales las ponemos juntas debajo
+h = int(input('introduce la altura del cilintro: '))
+radio = int(input('introduce el radio del círculo: '))
+
+# invocamos a las funciones todas juntas debajo del todo
+print (area(radio))
+print (f'el volumen de un cilintro con altura {h} y radio {radio} es {volumen(area(radio))}')
 
